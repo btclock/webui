2a7ba588e2ab06d433e9ac3f6882bfd4a7f714fe
Djuri Baars <dsbaars@users.noreply.github.com>
2024-06-09 00:34:10 +0200

    Fix window error for tests

--- src/lib/screen.ts
+++ src/lib/screen.ts
@@ -1,9 +1,18 @@
 import { writable } from 'svelte/store';
 
+// Check if window is available
+let initialWidth: number = 0;
+if (typeof window !== 'undefined') {
+	initialWidth = window.innerWidth;
+}
+
 // Create a writable store to track screen size
-export const screenSize = writable<number>(window.innerWidth);
+export const screenSize = writable<number>(initialWidth);
 
 // Function to update the screen size
-export const updateScreenSize = () => {
-	screenSize.set(window.innerWidth);
+export const updateScreenSize = (): void => {
+	// Check if window is available before setting the screen size
+	if (typeof window !== 'undefined') {
+		screenSize.set(window.innerWidth);
+	}
 };
